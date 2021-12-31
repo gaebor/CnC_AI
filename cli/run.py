84b699fa23b582a20ce670d048e81d4d9874925f
Gábor Borbély <gaebor@gmail.com>
2021-12-31 19:00:28 +0100

    refactor start game

--- cli/run.py
+++ cli/run.py
@@ -4,6 +4,7 @@ import os
 from random import getrandbits, sample
 
 import cnc_structs
+import decoders
 
 
 def get_args():
@@ -20,6 +21,8 @@ def get_args():
 
 class TDGameplay:
     def __init__(self, dll_path, content_directory):
+        self.players = []
+        self.content_directory = content_directory
         self.dll = ctypes.WinDLL(dll_path)
         self.difficulties = cnc_structs.get_diff()
         self.dll.CNC_Init(ctypes.c_char_p(b"-CD\"" + content_directory + b"\""), None)
@@ -44,6 +47,61 @@ class TDGameplay:
         )(('CNC_Get_Game_State', self.dll))
         self.game_state_buffer = (ctypes.c_uint8 * (4 * 1024 ** 2))()
 
+    def add_player(self, playerinfo: cnc_structs.CNCPlayerInfoStruct):
+        self.players.append(playerinfo)
+
+    def start_game(
+        self, multiplayer_options: cnc_structs.CNCMultiplayerOptionsStruct, scenario_index
+    ):
+        self.players = (cnc_structs.CNCPlayerInfoStruct * len(self.players))(*self.players)
+        if False == self.dll.CNC_Set_Multiplayer_Data(
+            scenario_index,
+            ctypes.byref(multiplayer_options),
+            ctypes.c_int(len(self.players)),
+            self.players,
+            ctypes.c_int(6),  # max number of players
+        ):
+            raise ValueError('CNC_Set_Multiplayer_Data')
+
+        if False == self.dll.CNC_Start_Instance_Variation(
+            scenario_index,
+            ctypes.c_int(-1),  # scenario_variation
+            ctypes.c_int(0),  # scenario_direction
+            ctypes.c_int(7),  # build_level
+            ctypes.c_char_p(b"MULTI"),  # faction
+            ctypes.c_char_p(b"GAME_GLYPHX_MULTIPLAYER"),  # game_type
+            ctypes.c_char_p(self.content_directory),
+            ctypes.c_int(-1),  # sabotaged_structure
+            ctypes.c_char_p(b""),  # override_map_name
+        ):
+            raise ValueError('CNC_Start_Instance_Variation')
+
+        self.dll.CNC_Set_Difficulty(ctypes.c_int(0))
+
+        for player in self.players:
+            StartLocationIndex = ctypes.c_int()
+            if self.dll.CNC_Get_Start_Game_Info(
+                ctypes.c_uint64(player.GlyphxPlayerID), ctypes.byref(StartLocationIndex)
+            ):
+                player.StartLocationIndex = StartLocationIndex.value
+            else:
+                raise ValueError('CNC_Get_Start_Game_Info')
+
+        self.dll.CNC_Handle_Game_Request(ctypes.c_int(1))  # INPUT_GAME_LOADING_DONE
+        self.retrieve_players_info()
+
+    def retrieve_players_info(self):
+        # overrides House info GOOD/BAD becomes MULTI1-6
+        for player in self.players:
+            if not self.CNC_Get_Game_State(
+                cnc_structs.GameStateRequestEnum['GAME_STATE_PLAYER_INFO'][0],
+                player.GlyphxPlayerID,
+                ctypes.cast(ctypes.pointer(player), ctypes.POINTER(ctypes.c_ubyte)),
+                ctypes.sizeof(cnc_structs.CNCPlayerInfoStruct)
+                + 33,  # A little extra for no reason
+            ):
+                raise ValueError('CNC_Get_Game_State')
+
     def get_game_state(self, state_request, player_id):
         request_type, result_type = cnc_structs.GameStateRequestEnum[state_request]
         if self.CNC_Get_Game_State(
@@ -61,16 +119,13 @@ class TDGameplay:
 def main(args):
     os.chdir(args.dir)
 
-    content_directory = b'DATA\\CNCDATA\\TIBERIAN_DAWN\\CD1'
-    TD = TDGameplay(args.dll_path, content_directory)
+    TD = TDGameplay(args.dll_path, b'DATA\\CNCDATA\\TIBERIAN_DAWN\\CD1')
 
     houses = getrandbits(6)
     houses = tuple((houses >> i) % 2 for i in range(6))
     colors = sample([0, 1, 2, 3, 4, 5], k=6)
 
-    scenario_index = ctypes.c_int(50)
-
-    players = [
+    TD.add_player(
         cnc_structs.CNCPlayerInfoStruct(
             GlyphxPlayerID=1055504538,
             Name=b"ai1",
@@ -80,7 +135,9 @@ def main(args):
             ColorIndex=colors[1],
             IsAI=True,
             StartLocationIndex=127,
-        ),
+        )
+    )
+    TD.add_player(
         cnc_structs.CNCPlayerInfoStruct(
             GlyphxPlayerID=76561199154512029,
             Name=b"gaebor",
@@ -90,9 +147,8 @@ def main(args):
             ColorIndex=colors[0],
             IsAI=False,
             StartLocationIndex=127,
-        ),
-    ]
-    players = (cnc_structs.CNCPlayerInfoStruct * len(players))(*players)
+        )
+    )
 
     multiplayer_options = cnc_structs.CNCMultiplayerOptionsStruct(
         MPlayerCount=2,
@@ -112,40 +168,8 @@ def main(args):
         DestroyStructures=False,
         ModernBalance=True,
     )
-    if False == TD.dll.CNC_Set_Multiplayer_Data(
-        scenario_index,
-        ctypes.byref(multiplayer_options),
-        ctypes.c_int(len(players)),
-        players,
-        ctypes.c_int(6),  # max number of players
-    ):
-        raise ValueError('CNC_Set_Multiplayer_Data')
 
-    if False == TD.dll.CNC_Start_Instance_Variation(
-        scenario_index,
-        ctypes.c_int(-1),  # scenario_variation
-        ctypes.c_int(0),  # scenario_direction
-        ctypes.c_int(7),  # build_level
-        ctypes.c_char_p(b"MULTI"),  # faction
-        ctypes.c_char_p(b"GAME_GLYPHX_MULTIPLAYER"),  # game_type
-        ctypes.c_char_p(content_directory),
-        ctypes.c_int(-1),  # sabotaged_structure
-        ctypes.c_char_p(b""),  # override_map_name
-    ):
-        raise ValueError('CNC_Start_Instance_Variation')
-
-    TD.dll.CNC_Set_Difficulty(ctypes.c_int(0))
-
-    for player in players:
-        StartLocationIndex = ctypes.c_int()
-        if TD.dll.CNC_Get_Start_Game_Info(
-            ctypes.c_uint64(player.GlyphxPlayerID), ctypes.byref(StartLocationIndex)
-        ):
-            player.StartLocationIndex = StartLocationIndex.value
-        else:
-            raise ValueError('CNC_Get_Start_Game_Info')
-
-    TD.dll.CNC_Handle_Game_Request(ctypes.c_int(1))  # INPUT_GAME_LOADING_DONE
+    TD.start_game(multiplayer_options, ctypes.c_int(50))
 
     width, height = ctypes.c_uint(0), ctypes.c_uint(0)
     image_buffer = (ctypes.c_uint8 * ((64 * 24) * (64 * 24)))()
@@ -156,19 +180,19 @@ def main(args):
     else:
         raise ValueError('CNC_Get_Palette')
 
-    TD.get_game_state('GAME_STATE_STATIC_MAP', 0)
+    map = TD.get_game_state('GAME_STATE_STATIC_MAP', 0)
     frame = 1
     while TD.Advance_Instance(ctypes.c_uint64(0)):
         print(frame, end='\r')
         if frame % 1000 == 0:
-            TD.get_game_state('GAME_STATE_DYNAMIC_MAP', 0)
-            TD.get_game_state('GAME_STATE_LAYERS', 0)
+            dynamic_map = TD.get_game_state('GAME_STATE_DYNAMIC_MAP', 0)
+            layers = TD.get_game_state('GAME_STATE_LAYERS', 0)
             TD.get_game_state('GAME_STATE_OCCUPIER', 0)
 
-            for player in players:
-                TD.get_game_state('GAME_STATE_SIDEBAR', player.GlyphxPlayerID)
-                TD.get_game_state('GAME_STATE_SHROUD', player.GlyphxPlayerID)
-                TD.get_game_state('GAME_STATE_PLACEMENT', player.GlyphxPlayerID)
+            # for player in players:
+            #     TD.get_game_state('GAME_STATE_SIDEBAR', player.GlyphxPlayerID)
+            #     TD.get_game_state('GAME_STATE_SHROUD', player.GlyphxPlayerID)
+            #     TD.get_game_state('GAME_STATE_PLACEMENT', player.GlyphxPlayerID)
 
             if TD.dll.CNC_Get_Visible_Page(
                 image_buffer, ctypes.byref(width), ctypes.byref(height)
@@ -179,8 +203,9 @@ def main(args):
                 # img.show()
         frame += 1
     print()
-    for player in players:
-        print(TD.get_game_state('GAME_STATE_PLAYER_INFO', player.GlyphxPlayerID))
+    TD.retrieve_players_info()
+    for player in TD.players:
+        print(player)
 
 
 if __name__ == '__main__':
