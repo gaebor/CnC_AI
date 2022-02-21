b47f58fee5b59555fc2279a0e18edb659bb8bd93
Gábor Borbély <gaebor@gmail.com>
2022-02-21 22:55:00 +0100

    refactor and reshape better (sidebar may be zero length)

--- DllWrapper/TIBERIANDAWN/vectorization.py
+++ DllWrapper/TIBERIANDAWN/vectorization.py
@@ -5,29 +5,54 @@ import numpy
 import cnc_structs
 
 
+def read_array_from_buffer(buffer, offset, Type):
+    count = ctypes.c_uint32.from_buffer_copy(buffer, offset).value
+    offset += ctypes.sizeof(ctypes.c_uint32)
+    number_of_members = ctypes.sizeof(Type) // 4
+    indices = numpy.frombuffer(
+        buffer, dtype='uint32', count=count * number_of_members, offset=offset
+    ).reshape(count, number_of_members)
+    continuous = numpy.frombuffer(
+        buffer, dtype='float32', count=count * number_of_members, offset=offset
+    ).reshape(count, number_of_members)
+    offset += count * ctypes.sizeof(Type)
+    return offset, indices, continuous
+
+
 def convert_to_np(game_state_buffer):
     offset = 8 * ctypes.sizeof(ctypes.c_int)
     map_cells = numpy.frombuffer(
         game_state_buffer, dtype='uint32', count=2 * 62 * 62, offset=offset
     )
-    asset_names = map_cells[0::2].reshape((62, 62))
-    shape_indices = map_cells[1::2].reshape((62, 62))
-
     offset = ctypes.sizeof(cnc_structs.StaticMap)
-    dynamic_objects_count = ctypes.c_uint32.from_buffer_copy(game_state_buffer, offset).value
-    offset += ctypes.sizeof(ctypes.c_uint32)
-    dynamic_objects_indices = numpy.frombuffer(
-        game_state_buffer,
-        dtype='uint32',
-        count=dynamic_objects_count * ctypes.sizeof(cnc_structs.DynamicObject) // 4,
-        offset=offset,
-    ).reshape(dynamic_objects_count, -1)[:, :-5]
 
-    dynamic_objects_continuous = numpy.frombuffer(
+    offset, dynamic_objects_indices, dynamic_objects_continuous = read_array_from_buffer(
+        game_state_buffer, offset, cnc_structs.DynamicObject
+    )
+
+    sidebar_members = numpy.frombuffer(
         game_state_buffer,
         dtype='float32',
-        count=dynamic_objects_count * ctypes.sizeof(cnc_structs.DynamicObject) // 4,
+        count=ctypes.sizeof(cnc_structs.SideBarMembers) // 4,
         offset=offset,
-    ).reshape(dynamic_objects_count, -1)[:, -5:]
+    )
+    offset += ctypes.sizeof(cnc_structs.SideBarMembers)
 
-    return dynamic_objects_continuous
+    offset, sidebar_entries_indices, sidebar_entries_continuous = read_array_from_buffer(
+        game_state_buffer, offset, cnc_structs.SidebarEntry
+    )
+
+    return {
+        'StaticAssetName': map_cells[0::2].reshape((62, 62)),
+        'StaticShapeIndex': map_cells[1::2].reshape((62, 62)),
+        'AssetName': dynamic_objects_indices[:, 0],
+        'ShapeIndex': dynamic_objects_indices[:, 1],
+        'Owner': dynamic_objects_indices[:, 2],
+        'Pips': dynamic_objects_indices[:, 3 : 3 + cnc_structs.MAX_OBJECT_PIPS],
+        'ControlGroup': dynamic_objects_indices[:, 3 + cnc_structs.MAX_OBJECT_PIPS],
+        'Cloak': dynamic_objects_indices[:, 3 + cnc_structs.MAX_OBJECT_PIPS + 1],
+        'Continuous': dynamic_objects_continuous[:, -5:],
+        'SidebarInfos': sidebar_members,
+        'SidebarAssetName': sidebar_entries_indices[:, 0],
+        'SidebarContinuous': sidebar_entries_continuous[:, 3:],
+    }
