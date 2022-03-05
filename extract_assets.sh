f31dddce78440dd44910c27109efac57b33cd391
Gábor Borbély <gaebor@gmail.com>
2022-03-05 22:25:17 +0100

    add script that extracts asset names ans shape indices

---
+++ extract_assets.sh
@@ -0,0 +1,28 @@
+# use bigeditor (https://www.ppmsite.com/osbigeditorinfo/) to extract from .MEG files
+
+function parse_name {
+    filename="$1"
+    sed 's/\([-a-zA-Z0-9]\+\)\(.[A-Z]\+\)\?-\([0-9]\+\)\(-[0-9]\+\)\?/\1\t\3/'<<<"${filename%.*}"
+    # sed 's/([a-zA-Z0-9]+)(.[A-Z]+)?-([0-9]+)(-[0-9]+)?/\1\t\3/'<<<"$fileame"
+}
+
+dirname=.
+if [ "$1" ]
+then
+    dirname="$1"
+fi
+
+(
+for file in `find "$dirname" -iname '*.tga' -type f` `find "$dirname" -iname '*.dds' -type f`
+do
+    parse_name `basename $file`
+done
+
+for zip_file in `find "$dirname" -iname '*.zip' -type f`
+do
+    for file in `unzip -l $zip_file | tail +4 | head -n -2 | sed 's/  \+/\t/g' | cut -f4 | grep -i tga`
+    do
+        parse_name $file
+    done
+done
+) | sort | uniq | cut -f1 | uniq -c | sort -rn
