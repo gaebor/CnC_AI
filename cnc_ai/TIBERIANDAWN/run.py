905c96624e08fb00e0a0c3aebf5497237c34bd4d
Gábor Borbély <gaebor@gmail.com>
2022-06-18 15:53:17 +0200

    yet an other command line argument

--- cnc_ai/TIBERIANDAWN/run.py
+++ cnc_ai/TIBERIANDAWN/run.py
@@ -42,16 +42,20 @@ def get_args():
         type=int,
         help='stop game after this many iterations if still going',
     )
-    parser.add_argument('--exe', default='TIBERIANDAWN_wrapper.exe', help='absolute path')
+    parser.add_argument(
+        '--exe', default='TIBERIANDAWN_wrapper.exe', metavar='absolute path', help=' '
+    )
     parser.add_argument(
         '--dir',
         default=r'C:\Program Files (x86)\Steam\steamapps\common\CnCRemastered',
-        help='absolute path',
+        metavar='absolute path',
+        help='The game installation directory. '
+        'Buy the game on Steam: https://store.steampowered.com/app/1213210',
     )
     parser.add_argument(
         '--dll',
         default='TiberianDawn.dll',
-        help='path to the game DLL, absolute or relative from `dir`. '
+        help='Path to the game DLL, absolute or relative from `dir`. '
         'This DLL is released with the game itself but it was opensourced too: '
         'https://github.com/electronicarts/CnC_Remastered_Collection',
     )
@@ -59,29 +63,37 @@ def get_args():
     parser.add_argument(
         '--load',
         default='',
-        help='loads model from pytorch model file '
-        'rather than initializing a new one on the beginning',
+        help='Load model from pytorch model file '
+        'rather than initializing a new one on the beginning.',
     )
     parser.add_argument(
-        '--save', default='', help='save model to pytorch model file after the game(s)'
+        '--save', default='', help='Save model to pytorch model file after the game(s).'
     )
     parser.add_argument(
         '-D',
         dest='spawn',
         default=True,
         action='store_false',
-        help="don't spawn subprocesses to run the DLL (set by '--exe'). "
-        "this is good for debugging when you start the dll wrapper separately.",
+        help="Don't spawn subprocesses to run the DLL (set by '--exe'). "
+        "This is good for debugging when you start the dll wrapper separately.",
     )
     parser.add_argument(
         '-P',
         '--print',
         default=False,
         action='store_true',
-        help="print out what was the terminal state of the game(s)",
+        help="Print out what was the terminal state of the game(s).",
     )
     parser.add_argument(
-        '-T', '--train', default=False, action='store_true', help="train at the end of the game"
+        '-T', '--train', default=False, action='store_true', help="Train at the end of the game."
+    )
+    parser.add_argument(
+        '-r',
+        '--record',
+        default=False,
+        action='store_true',
+        help="Save recording of the games for later inspection or training. "
+        "If you train at the end of the games than save recording may not be necessary.",
     )
     return parser.parse_args()
 
@@ -178,8 +190,6 @@ class GameHandler(tornado.websocket.WebSocketHandler):
     def end_game(self):
         if self.loser_mask == 0:
             self.loser_mask = self.assess_players_performance()
-        if self.loser_mask > 0:
-            self.save_gameplay()
 
     def init_game(self):
         self.folder = datetime.now().strftime("%Y-%m-%d_%Hh%Mm%S.%fs") + '_' + str(id(self))
@@ -365,6 +375,9 @@ def main():
             for i in range(len(game.players)):
                 print(i)
                 game.print_what_player_sees(i)
+    if args.record:
+        for game in GameHandler.games:
+            game.save_gameplay()
     if args.train:
         GameHandler.train()
     if args.save:
