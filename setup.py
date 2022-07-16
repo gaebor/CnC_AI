9f3b30f1c75beb7cc9dc188badc2cf1826c57162
Gábor Borbély <gaebor@gmail.com>
2022-07-16 21:29:47 +0200

    try to detect gpu at install

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+from subprocess import run, PIPE
+from re import search
+
+INSTALL_REQUIRES = ['numpy', 'tornado', 'termcolor', 'tqdm']
+nvidia_smi_output = run(
+    'nvidia-smi', text=True, shell=True, check=False, stdout=PIPE, stderr=PIPE
+).stdout
+version_match = search('CUDA Version: ((\d+)\.(\d+))', nvidia_smi_output)
+if version_match:
+    cuda_version_str = 'cu' + version_match.group(2) + version_match.group(3)
+    dependency_links = ['https://download.pytorch.org/whl/' + cuda_version_str]
+    INSTALL_REQUIRES.append(f'torch>=1.9.1+{cuda_version_str}')
+else:
+    dependency_links = []
+    INSTALL_REQUIRES.append('torch')
+
 
 setup(
     name='cnc_ai',
@@ -11,7 +27,8 @@ setup(
     author_email='borbely@math.bme.hu',
     url='https://github.com/gaebor/CnC_AI',
     license='MIT',
-    install_requires=['numpy', 'torch', 'tornado', 'termcolor', 'tqdm'],
+    install_requires=INSTALL_REQUIRES,
     packages=['cnc_ai', 'cnc_ai.TIBERIANDAWN'],
     include_package_data=True,
+    dependency_links=dependency_links,
 )
